590fc9a66e001030e7570c175c076cbdf766f54f
itshiroto <rivowowor7@gmail.com>
2022-05-20 23:25:16 +0800

    Testing tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,9 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div className="App container mx-auto h-screen flex items-center justify-center flex-col gap-5">
+      <h1 className="text-5xl font-bold underline">Hello gaes</h1>
+      <p className="text-xl">Created using Vite + React + TailwindCSS</p>
+      <p className="italic">Sekarang kerjakan tugasnya - <a href="https://github.com/itshiroto" className="text-blue-600">itshiroto</a></p>
     </div>
   )
 }
