e55433e6f726d8a622a61a139c1187362a21e044
vools <auliyaav@gmail.com>
2022-05-27 16:20:15 +0700

    membuat greetings bar, top bar + icons bersama jaki,pelik
    Co-authored-by: Fadhil Dzaky <FFreezed@users.noreply.github.com>
    Co-authored-by: Felix <pelikk@users.noreply.github.com>

--- src/components/GreetingsBar/GreetingsBar.tsx
+++ src/components/GreetingsBar/GreetingsBar.tsx
@@ -1,5 +1,5 @@
+import { Icon } from '@iconify/react';
 // TODO bikin supaya div utamanya supaya sizenya ngikutin size anakannya
-// TODO bikin icon news di sebelah kanan div parentnya
 
 type GreetingsProps = {
   currentTime?: string, // TODO ganti nanti ke time
@@ -7,14 +7,27 @@ type GreetingsProps = {
   userMajor: string,
 }
 
+type ButtonProps = {
+  onClick?: () => void,
+}
+
+const NewsButton = (
+  {onClick} : ButtonProps
+) => (
+  <div onClick={onClick} className="flex ml-auto mt-2 mb-4 rounded-lg bg-slate-50 cursor-pointer transition hover:text-slate-500 text-gray-700">
+    <Icon icon="fluent:news-24-filled" width="60" height="60" />
+  </div>
+)
+
 export const GreetingsBar: React.FunctionComponent<GreetingsProps> = ({ userName, userMajor }) => {
   return (
-    <div className="w-full flex flex-col px-4 py-2 bg-gray-700 text-white rounded-lg">
-      <div>
-        <h3 className="text-3xl font-bold">Hello,</h3>
+    <div className="lg:w-96 flex flex-row px-4 py-2 bg-gray-700 text-white rounded-lg">
+      <div className="flex-col">
+        <h3 className="lg:text-3xl font-bold">Good Morning</h3>
         <p>{userName}</p>
         <p className="italic">{userMajor}</p>
       </div>
+      <NewsButton onClick={() => console.log("tid")} />
     </div>
   )
 }
