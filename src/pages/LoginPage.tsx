2cd1b427f58f9490ccbb61f488ad3146b0ca83b0
Padeel 15cm <fadhil.dzaky@student.umn.ac.id>
2022-05-30 01:30:24 +0700

    Login Page, Belum Rapih Ngab

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -28,33 +28,27 @@ export const LoginPage = () => {
       <div className="fixed top-0 right-0 p-5">
         <AboutButton /> {/* TODO: kasih Modal */}
       </div>
-      <h1 className="text-center text-red-600">Halo semua Aul suka yg pas ditangan</h1>
+      <h1 className="text-center text-red-600">Halo semua, Aul suka yg pas ditangan</h1>
       <h1 className="text-center text-6xl">Choose Your Titit</h1>
-      <div className="grow-1">
-        <div className="flex flex-row">
+      <div className="">
+        <div className="md:flex flex-row justify-center mt-14 mx-14">
           <img src={avatar} className="object-contain h-96 w-96"/>
           <div className="">
-            <input type="name" name="nama" className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-96 rounded-md sm:text-sm focus:ring-1" placeholder="Nama"/>
-            <div className="flex flex-row">
+            <input type="name" name="nama" className="mt-5 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-80 rounded-md sm:text-sm focus:ring-1" placeholder="Nama"/>
+            <div className="flex flex-row my-4">
               <input type="name" name="nama" className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-60 rounded-md sm:text-sm focus:ring-1" placeholder="Jurusan"/>
-              <button id="dropdownDefault" data-dropdown-toggle="dropdown" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">Dropdown button</button>
-                <div id="dropdown" className="z-10 hidden bg-white divide-y divide-gray-100 rounded shadow w-44 dark:bg-gray-700">
-                  <ul className="py-1 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefault">
-                    <li>
-                      <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
-                    </li>
-                    <li>
-                      <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</a>
-                    </li>
-                    <li>
-                      <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</a>
-                    </li>
-                    <li>
-                      <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Sign out</a>
-                    </li>
-                  </ul>
-                </div>
+              <div className="dropdown inline-block relative m-1">
+                <button className="bg-gray-300 text-gray-700 font-semibold py-2 px-4 rounded inline-flex items-center">
+                  <span className="mr-1">Pilih</span>
+                  <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/> </svg>
+                </button>
+                <ul className="dropdown-menu absolute hidden text-gray-700 pt-1">
+                  <li className=""><a className=" bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap" href="#">Informatitid</a></li>
+                  <li className=""><a className="bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap" href="#">Ilmu Komunikatid</a></li>
+                  <li className=""><a className=" bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap" href="#">Sitem Informatid</a></li>
+                </ul>
               </div>
+            </div>
             <Button>Start</Button>
           </div>
         </div>
